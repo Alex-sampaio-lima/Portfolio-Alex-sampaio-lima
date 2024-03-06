922b9c53c870d0873d6b91fb8f6c9dc2e5377409
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-03-06 20:19:40 -0300

    feat: create React base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HomePage } from "./pages/HomePage"
+import ".//components/styles/index.scss"
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
