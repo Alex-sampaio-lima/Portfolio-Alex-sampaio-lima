6e8def7d6961f080b13420cd7ba31da739b7ce85
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-03-12 19:56:56 -0300

    feat: Creating Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import style from "./style.module.scss"
+import myLogo from "../../assets/myLogo.png"
+
+export function Header() {
+    return (
+        <header>
+            <div className={`${style.headerContainer} container`} >
+                <img className={style.imgMyLogo} src={myLogo} alt="Imagem da minha logo" />
+                <div className={style.titleContainer}>
+                    <a className="title1 border" href="#aboutSection">Sobre</a>
+                    <a className="title1 border" href="#tecnoSection">Stack</a>
+                    <a className="title1 border" href="#projectSection">Projetos</a>
+                </div>
+                <a className="btn" href="https://wa.me/5511940264105" target="_blank">Contato</a>
+            </div>
+        </header >
+    )
+}
