3e79c2aafece4d7a810360881a7e644299f28173
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-03-12 21:23:49 -0300

    feat: add text about me

--- src/components/sections/AboutMeSection/index.jsx
+++ src/components/sections/AboutMeSection/index.jsx
@@ -8,7 +8,8 @@ export function AboutMe() {
                     <h1 className="title2">Sobre mim</h1>
                 </div>
                 <div className={style.paragraphContainer}>
-                    <p className="paragraph aboutMeFormatted">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nemo cum ipsum omnis assumenda fugiat, architecto accusamus nesciunt magni perspiciatis rem veniam neque sapiente voluptatem similique blanditiis voluptatibus error a! </p>
+                    <p className="paragraph aboutMeFormatted littleText">
+                        Um desenvolvedor Full Stack e também um apreciador de games, sou uma pessoa curiosa desde criança, que sempre tenta entender como as coisas funcionam e o seu porquê. Comecei estudar programação pois via muitas pessoas falando sobre essa profissão e também pois sempre quis entender esse mundo que nos filmes e mídia aparentava ser um “bicho de sete cabeças” e algo que só pessoas muito inteligentes conseguiriam realizar. Ao ter o primeiro contato percebi que era complicado mas não impossível, comecei com cursos básicos até um mais avançado e hoje em dia já realizei mais de 40 projetos entre back e fron end. </p>
                 </div>
             </div>
         </section>
